d2984a203a3259ebef4dc8092765cb0366129a05
AikoN76 <vincenzo.squillante@gmail.com>
2020-02-01 16:59:24 +0100

    first commit

---
+++ src/test/java/guru/springframework/msscbrewery/web/controller/BeerControllerTest.java
@@ -0,0 +1,147 @@
+package guru.springframework.msscbrewery.web.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import guru.springframework.msscbrewery.services.BeerService;
+import guru.springframework.msscbrewery.web.model.BeerDto;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.constraints.ConstraintDescriptions;
+import org.springframework.restdocs.payload.FieldDescriptor;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.StringUtils;
+
+import java.util.UUID;
+
+import static org.hamcrest.core.Is.is;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.BDDMockito.given;
+import static org.mockito.BDDMockito.then;
+//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.request.RequestDocumentation.*;
+import static org.springframework.restdocs.snippet.Attributes.key;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "dev.springframework.guru", uriPort = 80)
+@WebMvcTest(BeerController.class)
+@ComponentScan(basePackages = "guru.springframework.msscbrewery.web.mappers")
+public class BeerControllerTest {
+
+    @MockBean
+    BeerService beerService;
+
+    @Autowired
+    MockMvc mockMvc;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    BeerDto validBeer;
+
+    @BeforeEach
+    public void setUp() {
+        validBeer = BeerDto.builder().id(UUID.randomUUID())
+                .beerName("Beer1")
+                .beerStyle("PALE_ALE")
+                .upc(123456789012L)
+                .build();
+    }
+
+    @Test
+    public void getBeer() throws Exception {
+        given(beerService.getBeerById(any(UUID.class))).willReturn(validBeer);
+
+        mockMvc.perform(get("/api/v1/beer/{beerId}", validBeer.getId().toString())
+                .param("isRed","no")
+                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON_UTF8))
+                .andExpect(jsonPath("$.id", is(validBeer.getId().toString())))
+                .andExpect(jsonPath("$.beerName", is("Beer1")))
+                .andDo(document("v1/beer-get", pathParameters(
+                        parameterWithName("beerId").description("UUID of the desired beer to get")
+                ),
+                        requestParameters(
+                                parameterWithName("isRed").description("Is the Beer red query param")
+                        ),
+                        responseFields(
+                                fieldWithPath("id").description("UUID of the Beer"),
+                                fieldWithPath("beerName").description("Beer Name"),
+                                fieldWithPath("beerStyle").description("Beer Style"),
+                                fieldWithPath("upc").description("UPC of the bee"),
+                                fieldWithPath("createdDate").description("Date Created"),
+                                fieldWithPath("lastUpdatedDate").description("Date Updated")
+                        )));
+    }
+
+    @Test
+    public void handlePost() throws Exception {
+        //given
+        BeerDto beerDto = validBeer;
+        beerDto.setId(null);
+        BeerDto savedDto = BeerDto.builder().id(UUID.randomUUID()).beerName("New Beer").build();
+        String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+
+        given(beerService.saveNewBeer(any())).willReturn(savedDto);
+
+        ConstrainedFields fields = new ConstrainedFields(BeerDto.class);
+
+        mockMvc.perform(post("/api/v1/beer/")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(beerDtoJson))
+                .andExpect(status().isCreated())
+                .andDo(document("v1/beer-new",
+                            requestFields(
+                                    fields.withPath("id").ignored(),
+                                    fields.withPath("createdDate").ignored(),
+                                    fields.withPath("lastUpdatedDate").ignored(),
+                                    fields.withPath("beerName").description("Name of the Beer"),
+                                    fields.withPath("beerStyle").description("Style of the beer"),
+                                    fields.withPath("upc").description("Beer UPC").attributes()
+                            )));
+
+    }
+
+    @Test
+    public void handleUpdate() throws Exception {
+        //given
+        BeerDto beerDto = validBeer;
+        beerDto.setId(null);
+        String beerDtoJson = objectMapper.writeValueAsString(beerDto);
+
+        //when
+        mockMvc.perform(put("/api/v1/beer/" + UUID.randomUUID())
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(beerDtoJson))
+                .andExpect(status().isNoContent());
+
+        then(beerService).should().updateBeer(any(), any());
+
+    }
+
+    private static class ConstrainedFields {
+
+        private final ConstraintDescriptions constraintDescriptions;
+
+        ConstrainedFields(Class<?> input){
+            this.constraintDescriptions = new ConstraintDescriptions(input);
+        }
+
+        private FieldDescriptor withPath(String path){
+            return fieldWithPath(path).attributes(key("constraints").value(StringUtils.collectionToDelimitedString
+                    (this.constraintDescriptions.descriptionsForProperty(path), ". ")));
+        }
+    }
+}
